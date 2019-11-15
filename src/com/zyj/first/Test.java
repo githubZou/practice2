fbb440a1a6c3d0398c980e90db88915d458085ce
zou <415385352@qq.com>
2019-11-15 15:30:28 +0800

    IDEA提交

---
+++ src/com/zyj/first/Test.java
@@ -0,0 +1,10 @@
+package com.zyj.first;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub");
+        System.out.println("测试GitHub");
+        System.out.println("测试GitHub");
+        System.out.println("测试GitHub");
+    }
+}
